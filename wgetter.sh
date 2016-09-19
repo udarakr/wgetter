71878676406c0e0f6a344dc86228888efd3c07b8
udarakr <udarakr@gmail.com>
2016-09-19 11:48:03 +0530

    making duplicate snippet a function

--- wgetter.sh
+++ wgetter.sh
@@ -12,24 +12,29 @@ wgetterDir="$baseDir/wgetter/"
 
 list="$gitDir/youtube.list"
 
+loop(){
+	echo "Reading new download link - $line"
+    # youtube-dl $link
+    #if link contains "youtube" use youtube-dl else use wget
+    if [[ $1 == https://www.youtube.com/* ]]
+	then
+		echo "Initializing download from youtube-dl..."
+	    youtube-dl -f bestvideo+bestaudio --newline $1 
+	else
+		echo "Initializing download from wget..."
+		wget $1
+	fi
+    echo "$1" >> $2
+}
+
 if [ -f $wgetterDir$localList ]
 then
 echo "Localcopy exists"
 echo "Comparing $list with local $wgetterDir$localList"
 #grep -Fxvf $wgetterDir$localList $list
-while read -r line ; do
-    echo "Reading new download link - $line"
-    # youtube-dl $link
-    #if link contains "youtube" use youtube-dl else use wget
-    if [[ $line == https://www.youtube.com/* ]]
-	then
-		echo "Initializing download from youtube-dl..."
-	    # youtube-dl $link
-	else
-		echo "Initializing download from wget..."
-		# wget $link
-	fi
-    echo "$line" >> $wgetterDir$localList
+while read -r line
+do
+	loop $line $wgetterDir$localList
 done < <(grep -Fxvf $wgetterDir$localList $list)
 else
 echo "Localcopy not exists in $wgetterDir$localList, Starting a fresh download from $list....."
@@ -39,17 +44,6 @@ touch $wgetterDir$localList
 
 while read -r line
 do
-    echo "Download link read from file - $line"
-    # youtube-dl $link
-    #if link contains "youtube" use youtube-dl else use wget
-    if [[ $line == https://www.youtube.com/* ]]
-	then
-		echo "Initializing download from youtube-dl..."
-	    # youtube-dl $link
-	else
-		echo "Initializing download from wget..."
-		# wget $link
-	fi
-    echo "$line" >> $wgetterDir$localList
+	loop $line $wgetterDir$localList
 done < "$list"
 fi
